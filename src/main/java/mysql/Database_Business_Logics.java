da30e301a709a33f7936b3d2ecc24bcbf04e2738
manthanpl93 <manthanpl93@live.com>
2018-03-03 23:58:27 +0530

    show tables query done without json

--- src/main/java/mysql/Database_Business_Logics.java
+++ src/main/java/mysql/Database_Business_Logics.java
@@ -4,22 +4,26 @@ import java.sql.*;
 
 public class Database_Business_Logics {
 
-	public static String get_all_database_names() throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+	public static String get_all_database_names()
+			throws InstantiationException, IllegalAccessException, ClassNotFoundException {
 		String jsonResponse = null;
+		String sTableName = null;
 		try {
-
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost/", "root", "root");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysqlsync", "root", "root");
 
 			Statement st = con.createStatement();
 			String sql = ("show tables;");
 			ResultSet rs = st.executeQuery(sql);
-			if (rs.next()) {
-				String sTableName = rs.getString("Tables_in_mysqlsync");
+			while (rs.next()) {
+				sTableName = rs.getString("Tables_in_mysqlsync");
+				
+				/** Kashyap give me all table names in jsonResponse **/
+				
 			}
 			con.close();
 		} catch (SQLException ex) {
-
+			ex.printStackTrace();
 		}
 
 		return jsonResponse;
