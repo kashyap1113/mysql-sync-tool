00796a9c61fce67a893132fee1fa8e817f808c62
manthanpl93 <manthanpl93@live.com>
2018-03-18 12:22:21 +0530

    *

--- src/main/java/com/kashyap1113/mysqlsynctool/controller/DatabaseOperationsServlet.java
+++ src/main/java/com/kashyap1113/mysqlsynctool/controller/DatabaseOperationsServlet.java
@@ -23,18 +23,13 @@ public class DatabaseOperationsServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+ 
     public DatabaseOperationsServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
+ 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         // TODO Auto-generated method stub
